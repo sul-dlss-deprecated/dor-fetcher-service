5a0e1ddb7dd29e547d5c07d96bd3eb889da58138
Joe Atzberger <ohiocore@gmail.com>
2016-02-12 17:38:19 -0800

    Delete old cassettes and update field names in fixtures

---
+++ spec/fixtures/rename.pl
@@ -0,0 +1,17 @@
+#/usr/bin/perl
+#
+# run as:
+#   perl -pi rename.pl file_to_edit
+
+use 5.012;
+use warnings;
+use strict;
+
+next unless $_;
+s/\bpublished_dt\b/published_dttsim/g;
+s/\bidentityMetadata_objectType_t\b/objectType_ssim/g;
+s/\btitle_t\b/title_tesim/g;
+s/\bpublic_dc_title_t\b/public_dc_title_tesim/g;
+s/\bcatkey_id_t\b/catkey_id_ssim/g;
+s/\bis_member_of_collection_s\b/is_member_of_ssim/g;
+s/\bis_governed_by_s\b/is_governed_by_ssim/g;
