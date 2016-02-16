2bfe0d504edb028db779530acf02d5eb08b5a703
Joe Atzberger <ohiocore@gmail.com>
2016-02-16 15:34:13 -0800

    Cleanup spec

--- spec/features/collections_spec.rb
+++ spec/features/collections_spec.rb
@@ -10,19 +10,12 @@ describe('Collections Controller')  do
     VCR.use_cassette('all_collections_index_call') do
        visit collections_path
        response = JSON.parse(page.body)
-
        # Ensure All Four Collection Druids Are Present
        result_should_contain_druids(@fixture_data.accessioned_collection_druids, response[collections_key])
-
        # Ensure No Other Druids Are Present
        result_should_not_contain_druids(@fixture_data.accessioned_druids - @fixture_data.accessioned_collection_druids, response[collections_key])
-
-       # Ensure No Items Were Returned
-       expect(response[items_key]).to be nil
-
-       # Ensure No APOS Were Returned
-       expect(response[apos_key]).to be nil
-
+       expect(response[items_key]).to be_nil # Ensure No Items Were Returned
+       expect(response[apos_key]).to be_nil  # Ensure No APOS Were Returned
        # Verify the Counts
        verify_counts_section(response, {collections_key => @fixture_data.accessioned_collection_druids.size})
      end
@@ -30,23 +23,14 @@ describe('Collections Controller')  do
 
   it 'the index of Collections should respect :last_modified and return only Stafford' do
     VCR.use_cassette('last_modified_date_collections_index_call') do
-      solrparams = {:last_modified => last_mod_test_date_collections}
-      target_url = collections_path(solrparams)
-      visit target_url
+      visit collections_path(:last_modified => last_mod_test_date_collections)
       response = JSON.parse(page.body)
-
       # Ensure the Stafford Collection Druid is Present
       result_should_contain_druids(@fixture_data.stafford_collections_druids, response[collections_key])
-
       # Ensure No Other Collection Druids Are Present
       result_should_not_contain_druids(@fixture_data.accessioned_druids - @fixture_data.stafford_collections_druids, response[collections_key])
-
-      # Ensure No Items Were Returned
-      expect(response[items_key]).to be nil
-
-      # Ensure No APOS Were Returned
-      expect(response[apos_key]).to be nil
-
+      expect(response[items_key]).to be_nil # Ensure No Items Were Returned
+      expect(response[apos_key]).to be_nil  # Ensure No APOS Were Returned
       # Verify the Counts
       verify_counts_section(response, {collections_key => @fixture_data.stafford_collections_druids.size})
     end
@@ -54,23 +38,14 @@ describe('Collections Controller')  do
 
   it 'the index of Collections should respect :first_modified and return only Revs' do
   VCR.use_cassette('first_modified_date_collections_index_call') do
-      solrparams = {:first_modified => first_mod_test_date_collections}
-      target_url = collections_path(solrparams)
-      visit target_url
+      visit collections_path(:first_modified => first_mod_test_date_collections)
       response = JSON.parse(page.body)
-
       # Ensure All Three Revs Collection Druids Are Present
       result_should_contain_druids(@fixture_data.revs_collections_druids, response[collections_key])
-
       # Ensure Not Other Collections Are Present
       result_should_not_contain_druids(@fixture_data.accessioned_druids - @fixture_data.revs_collections_druids, response[collections_key])
-
-      # Ensure No Items Were Returned
-      expect(response[items_key]).to be nil
-
-      # Ensure No APOS Were Returned
-      expect(response[apos_key]).to be nil
-
+      expect(response[items_key]).to be_nil # Ensure No Items Were Returned
+      expect(response[apos_key]).to be_nil  # Ensure No APOS Were Returned
       # Verify the Counts
       verify_counts_section(response, {collections_key => @fixture_data.revs_collections_druids.size})
     end
@@ -90,20 +65,14 @@ end
       # Check For JSON
       visit collection_path(@fixture_data.top_level_revs_collection_druid)
       with_prefix_response = JSON.parse(page.body)
-
       visit collection_path(@fixture_data.top_level_revs_collection_druid.split(':')[1])
       no_prefix_response = JSON.parse(page.body)
-
       expect(with_prefix_response).to eq(no_prefix_response)
-
       # Check For XML
       visit collection_path(@fixture_data.top_level_revs_collection_druid, :format => 'xml')
       with_prefix_response = page.body
-
       visit collection_path(@fixture_data.top_level_revs_collection_druid.split(':')[1], :format => 'xml')
-      no_prefix_response = page.body
-
-      expect(with_prefix_response).to eq(no_prefix_response)
+      expect(with_prefix_response).to eq(page.body)
     end
   end
 
@@ -112,22 +81,15 @@ end
       visit collection_path(@fixture_data.top_level_revs_collection_druid)
       response = JSON.parse(page.body)
       exclude_druids = @fixture_data.revs_items_druids + @fixture_data.revs_collections_druids
-
       # Ensure All Revs Collection Druids Are Present
       result_should_contain_druids(@fixture_data.revs_collections_druids, response[collections_key])
-
       # Ensure Not Other Collections Are Present
       result_should_not_contain_druids(@fixture_data.accessioned_druids - exclude_druids, response[collections_key])
-
       # Ensure All Revs Items Are Present
       result_should_contain_druids(@fixture_data.revs_items_druids, response[items_key])
-
       # Ensure No Other Items Are Present
       result_should_not_contain_druids(@fixture_data.accessioned_druids - exclude_druids, response[items_key])
-
-      # Ensure No APOS Are Present
-      expect(response[apos_key]).to be nil
-
+      expect(response[apos_key]).to be nil # Ensure No APOS Are Present
       # Verify the Counts
       verify_counts_section(response, {collections_key => @fixture_data.revs_collections_druids.size, items_key => @fixture_data.revs_items_druids.size})
     end
@@ -136,7 +98,6 @@ end
   it 'should only return a count of the Revs Druids when called with the count only parameter' do
     VCR.use_cassette('revs_collection_count_call') do
       visit collection_path(@fixture_data.top_level_revs_collection_druid, just_count_param)
-
       expect(page.body.to_i).to eq((@fixture_data.revs_items_druids + @fixture_data.revs_collections_druids).size)
     end
   end
@@ -160,13 +121,10 @@ end
       visit collection_path(@fixture_data.revs_subcollection_druid)
       response = JSON.parse(page.body)
       collections_list = [@fixture_data.revs_subcollection_druid]
-
       # Ensure Subcollection Druid is Present
       result_should_contain_druids(collections_list, response[collections_key])
-
       # Ensure No Other Collection Druids Are Present
       result_should_not_contain_druids(@fixture_data.accessioned_collection_druids - collections_list, response[collections_key])
-
       # Verify the Counts
       verify_counts_section(response, {collections_key => collections_list.size, items_key => 4})
     end
@@ -193,27 +151,16 @@ end
     VCR.use_cassette('revs_objects_dates', :allow_unused_http_interactions => true) do
      # All Revs Collection Objects Should Be Here
      # The Stafford Collection Object Should Not Be Here
-
-     # Set the dates
-     solrparams = {:first_modified => '2014-01-01T00:00:00Z', :last_modified => '2014-05-06T00:00:00Z'}
-     target_url = collections_path(solrparams)
-     visit target_url
+     visit collections_path(:first_modified => '2014-01-01T00:00:00Z', :last_modified => '2014-05-06T00:00:00Z')
      response = JSON.parse(page.body)
-
      # We Should Only Have The Three Revs Fixtures
      expect(response[counts_key][collections_key]).to eq(3)
-
      # Ensure The Three Revs Collection Driuds Are Present
      result_should_contain_druids(@fixture_data.revs_collections_druids, response[collections_key])
-
      # Ensure The Stafford Collection Druid Is Not Present
      result_should_not_contain_druids(@fixture_data.accessioned_collection_druids - @fixture_data.revs_collections_druids, response[collections_key])
-
-     # Ensure No Items Were Returned
-     expect(response[items_key]).to be nil
-
-     # Ensure No APOS Were Returned
-     expect(response[apos_key]).to be nil
+     expect(response[items_key]).to be nil # Ensure No Items Were Returned
+     expect(response[apos_key]).to be nil  # Ensure No APOS Were Returned
    end
   end
 end
