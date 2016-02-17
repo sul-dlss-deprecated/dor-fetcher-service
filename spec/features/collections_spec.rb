bcf4bfc34cc9941d4f8dcee2ff6554883e0aaf78
Joe Atzberger <ohiocore@gmail.com>
2016-02-16 16:26:05 -0800

    Yet another touch of cleanup

--- spec/features/collections_spec.rb
+++ spec/features/collections_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe('Collections Controller')  do
   before :each do
-    @fetcher = FetcherTester.new
     @fixture_data = FixtureData.new
   end
 
@@ -16,8 +15,7 @@ describe('Collections Controller')  do
        result_should_not_contain_druids(@fixture_data.accessioned_druids - @fixture_data.accessioned_collection_druids, response[collections_key])
        expect(response[items_key]).to be_nil # Ensure No Items Were Returned
        expect(response[apos_key]).to be_nil  # Ensure No APOS Were Returned
-       # Verify the Counts
-       verify_counts_section(response, {collections_key => @fixture_data.accessioned_collection_druids.size})
+       verify_counts_section(response, collections_key => @fixture_data.accessioned_collection_druids.size)
      end
   end
 
@@ -31,13 +29,12 @@ describe('Collections Controller')  do
       result_should_not_contain_druids(@fixture_data.accessioned_druids - @fixture_data.stafford_collections_druids, response[collections_key])
       expect(response[items_key]).to be_nil # Ensure No Items Were Returned
       expect(response[apos_key]).to be_nil  # Ensure No APOS Were Returned
-      # Verify the Counts
-      verify_counts_section(response, {collections_key => @fixture_data.stafford_collections_druids.size})
+      verify_counts_section(response, collections_key => @fixture_data.stafford_collections_druids.size)
     end
   end
 
   it 'the index of Collections should respect :first_modified and return only Revs' do
-  VCR.use_cassette('first_modified_date_collections_index_call') do
+    VCR.use_cassette('first_modified_date_collections_index_call') do
       visit collections_path(:first_modified => first_mod_test_date_collections)
       response = JSON.parse(page.body)
       # Ensure All Three Revs Collection Druids Are Present
@@ -46,10 +43,9 @@ describe('Collections Controller')  do
       result_should_not_contain_druids(@fixture_data.accessioned_druids - @fixture_data.revs_collections_druids, response[collections_key])
       expect(response[items_key]).to be_nil # Ensure No Items Were Returned
       expect(response[apos_key]).to be_nil  # Ensure No APOS Were Returned
-      # Verify the Counts
-      verify_counts_section(response, {collections_key => @fixture_data.revs_collections_druids.size})
+      verify_counts_section(response, collections_key => @fixture_data.revs_collections_druids.size)
     end
-end
+  end
 
   it 'find all objects even if not accessioned when you specify the correct parameter' do
     VCR.use_cassette('status=registered query') do
@@ -66,8 +62,7 @@ end
       visit collection_path(@fixture_data.top_level_revs_collection_druid)
       with_prefix_response = JSON.parse(page.body)
       visit collection_path(@fixture_data.top_level_revs_collection_druid.split(':')[1])
-      no_prefix_response = JSON.parse(page.body)
-      expect(with_prefix_response).to eq(no_prefix_response)
+      expect(with_prefix_response).to eq(JSON.parse(page.body))
       # Check For XML
       visit collection_path(@fixture_data.top_level_revs_collection_druid, :format => 'xml')
       with_prefix_response = page.body
@@ -89,9 +84,8 @@ end
       result_should_contain_druids(@fixture_data.revs_items_druids, response[items_key])
       # Ensure No Other Items Are Present
       result_should_not_contain_druids(@fixture_data.accessioned_druids - exclude_druids, response[items_key])
-      expect(response[apos_key]).to be nil # Ensure No APOS Are Present
-      # Verify the Counts
-      verify_counts_section(response, {collections_key => @fixture_data.revs_collections_druids.size, items_key => @fixture_data.revs_items_druids.size})
+      expect(response[apos_key]).to be_nil # Ensure No APOS Are Present
+      verify_counts_section(response, collections_key => @fixture_data.revs_collections_druids.size, items_key => @fixture_data.revs_items_druids.size)
     end
   end
 
@@ -125,8 +119,7 @@ end
       result_should_contain_druids(collections_list, response[collections_key])
       # Ensure No Other Collection Druids Are Present
       result_should_not_contain_druids(@fixture_data.accessioned_collection_druids - collections_list, response[collections_key])
-      # Verify the Counts
-      verify_counts_section(response, {collections_key => collections_list.size, items_key => 4})
+      verify_counts_section(response, collections_key => collections_list.size, items_key => 4)
     end
   end
 
@@ -134,14 +127,14 @@ end
     VCR.use_cassette('nt028fd5773 collection', :allow_unused_http_interactions => true) do
       visit collection_path(:id => 'nt028fd5773')
       response = JSON.parse(page.body)
-      expect(response['items'].size).to eq(8) # there should be 8 items in this collection
+      expect(response['items'].size).to eq(8)      # should be 8 items in this collection
       response['items'].each do |item|
-        if item['druid'] == 'druid:bb048rn5648' # this item is missing the title field in both places, and so the title should be blank
+        if item['druid'] == 'druid:bb048rn5648'    # this item is missing the title field in both places, and so the title should be blank
           expect(item['title']).to eq('')
         elsif item['druid'] == 'druid:bb113tm9924' # this item has the title field in the alternate spot, and should still come through ok
           expect(item['title']).to eq('Permatex 300 NASCAR Race: 1968')
         else
-          expect(item['title']).not_to eq('') # the rest should have a title
+          expect(item['title']).not_to eq('')      # the rest should have a title
         end
       end
     end
@@ -149,18 +142,17 @@ end
 
   it 'It should only return Revs collection objects between these two dates' do
     VCR.use_cassette('revs_objects_dates', :allow_unused_http_interactions => true) do
-     # All Revs Collection Objects Should Be Here
-     # The Stafford Collection Object Should Not Be Here
-     visit collections_path(:first_modified => '2014-01-01T00:00:00Z', :last_modified => '2014-05-06T00:00:00Z')
-     response = JSON.parse(page.body)
-     # We Should Only Have The Three Revs Fixtures
-     expect(response[counts_key][collections_key]).to eq(3)
-     # Ensure The Three Revs Collection Driuds Are Present
-     result_should_contain_druids(@fixture_data.revs_collections_druids, response[collections_key])
-     # Ensure The Stafford Collection Druid Is Not Present
-     result_should_not_contain_druids(@fixture_data.accessioned_collection_druids - @fixture_data.revs_collections_druids, response[collections_key])
-     expect(response[items_key]).to be nil # Ensure No Items Were Returned
-     expect(response[apos_key]).to be nil  # Ensure No APOS Were Returned
-   end
+      # All Revs Collection Objects Should Be Here
+      # The Stafford Collection Object Should Not Be Here
+      visit collections_path(:first_modified => '2014-01-01T00:00:00Z', :last_modified => '2014-05-06T00:00:00Z')
+      response = JSON.parse(page.body)
+      expect(response[counts_key][collections_key]).to eq(3) # Only Have The 3 Revs Fixtures
+      # Ensure The Three Revs Collection Driuds Are Present
+      result_should_contain_druids(@fixture_data.revs_collections_druids, response[collections_key])
+      # Ensure The Stafford Collection Druid Is Not Present
+      result_should_not_contain_druids(@fixture_data.accessioned_collection_druids - @fixture_data.revs_collections_druids, response[collections_key])
+      expect(response[items_key]).to be_nil # Ensure No Items Were Returned
+      expect(response[apos_key]).to be_nil  # Ensure No APOS Were Returned
+    end
   end
 end
