a732d5d2b57bca5694deadd4ffb7790e18d75387
Michael J. Giarlo <mjgiarlo@stanford.edu>
2018-12-07 13:43:36 -0800

    Run Rails 5.2 app:update task

--- config/application.rb
+++ config/application.rb
@@ -21,21 +21,6 @@ module DorFetcherService
     # Add in files in lib/, such as the fetcher module
     config.autoload_paths << Rails.root.join('lib')
 
-    # Added when upgrading from Rails 5.1 to 5.2. See the following:
-    #
-    # Leaving `ActiveRecord::ConnectionAdapters::SQLite3Adapter.represent_boolean_as_integer`
-    # set to false is deprecated. SQLite databases have used 't' and 'f' to serialize
-    # boolean values and must have old data converted to 1 and 0 (its native boolean
-    # serialization) before setting this flag to true. Conversion can be accomplished
-    # by setting up a rake task which runs
-    #
-    #   ExampleModel.where("boolean_column = 't'").update_all(boolean_column: 1)
-    #   ExampleModel.where("boolean_column = 'f'").update_all(boolean_column: 0)
-    #
-    # for all models and all boolean columns, after which the flag must be set to
-    # true by adding the following to your application.rb file
-    config.active_record.sqlite3.represent_boolean_as_integer = true
-
     # Config values set below here are used by the application
     config.app_name = 'DORFetcherService'
 
