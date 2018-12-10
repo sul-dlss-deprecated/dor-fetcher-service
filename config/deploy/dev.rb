c167754bbcbf13d10c72394c8d484ab6741ec5ac
Michael J. Giarlo <mjgiarlo@stanford.edu>
2018-12-10 15:52:41 -0600

    Remove deployment environment that does not exist

--- config/deploy/dev.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-set :bundle_without, %w[deployment test].join(' ')
-server 'dorfetcher-dev.stanford.edu', user: 'lyberadmin', roles: %w[web db app]
-
-Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, 'development'
